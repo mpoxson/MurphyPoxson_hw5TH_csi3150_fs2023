5b6b261a9002f6ef00b9f376a3fefe2b0831270e
mpoxson <mpoxson@gmail.com>
2023-11-08 11:28:24 -0500

    Start css

--- poxson_th5/src/components/TravelBlog/TravelBlog.js
+++ poxson_th5/src/components/TravelBlog/TravelBlog.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Travelblog.css";
 
 const TravelBlog = (props) => {
   return (
@@ -6,7 +7,7 @@ const TravelBlog = (props) => {
       <div>
         <h3>{props.place}</h3>
       </div>
-      <article>
+      <article className="imgCont">
         <img src={props.img1} alt={props.place} />
         <img src={props.img2} alt={props.place} />
         <img src={props.img3} alt={props.place} />
